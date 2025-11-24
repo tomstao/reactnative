3ada68597ad811c24fc52ec30f504915d1d26b01
Tao <96sutao@gmail.com>
2025-11-23 19:55:18 -0500

    feat: added simple UI

--- App.js
+++ App.js
@@ -8,10 +8,10 @@ export default function App() {
                     placeholder={'Your first goal'}
                     style={styles.textInput}
                 />
-                <Button title={'Add Goal'} />
+                <Button  title={'Add Goals'} />
             </View>
 
-            <View>
+            <View style={styles.goalsContainer}>
                 <Text>
                     List of goals...
                 </Text>
@@ -24,17 +24,27 @@ export default function App() {
 const styles = StyleSheet.create({
     appContainer: {
         padding: 50,
+        flex: 1,
     },
     inputContainer: {
+        flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
     },
 
     textInput: {
         borderWidth: 1,
         borderColor: '#ccc',
-        width: '80%',
+        width: '70%',
         marginRight: 8,
         padding: 8,
+    },
+
+    goalsContainer: {
+        flex: 4,
     }
 })
